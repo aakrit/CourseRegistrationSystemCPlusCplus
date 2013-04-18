0025bc26d776f9de4957fe4237f511b4712e1a34
Aakrit <asteroidxvi@gmail.com>
2013-04-18 18:38:40 -0500

    Initial Commit

---
+++ OODesign HW1.1_Singleton_Pattern/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OODesign HW1.1_Singleton_Pattern
+//
+//  Created by Aakrit Prasad on 4/18/13.
+//  Copyright (c) 2013 Aakrit Prasad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
